91979850713547bda8bb3e92fcdd4b6544b9c777
arunpatro <arun.kumar.patro@gmail.com>
2017-01-30 21:52:57 +0530

    add home page and format all html

--- kgpmods/urls.py
+++ kgpmods/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^courses/', include('courses.urls')),
+    url(r'^$', TemplateView.as_view(template_name='home.html'),
+        name='home'),
 ]
