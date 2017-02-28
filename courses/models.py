1c81d3e2ce040686be0742409e7d70ec40441724
arunpatro <arun.kumar.patro@gmail.com>
2017-02-28 18:14:25 +0530

    fetch content from db

--- courses/models.py
+++ courses/models.py
@@ -1,40 +1,14 @@
 from __future__ import unicode_literals
-
 from django.db import models
-
-class Page(models.Model):
-    title = models.CharField(max_length=128)
-    url = models.URLField()
-    views = models.IntegerField(default=0)
-
-    def __unicode__(self):
-        return self.title
-
-class Fruit(models.Model):
-    name = models.CharField(max_length=100, primary_key=True)
-    count = models.IntegerField(default=0)
-
-class Person(models.Model):
-    SHIRT_SIZES = (
-        ('S', 'Small'),
-        ('M', 'Medium'),
-        ('L', 'Large'),
-    )
-    name = models.CharField(max_length=60)
-    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
 
 class Course(models.Model):
     name = models.CharField(max_length=128)
-    id = models.CharField(max_length=7, primary_key=True)
-    # syllabus = models.CharField()
-    # books = models.ListField()
-    # prereq = models.ListField()
-    # grades = models.models.ListField()
-    # tags = models.ListField()
-    # comments = models.ListField()
-    views = models.IntegerField(default=0)
+    id = models.CharField(max_length=7, primary_key=True,unique=True)
+    credits = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0)
+    grades = models.CharField(max_length=100)
+    prereq = models.CharField(max_length=100)
+    syllabus = models.TextField()
 
     def __unicode__(self):
-        return self.title
-
-
+        return self.id
